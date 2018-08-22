bd3b1c340d05ee3f6d15fd34c68a5c322bf930e2
Kalvin Manson <kalvinmanson@gmail.com>
2018-08-22 10:46:29 -0500

    Frontend

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,61 @@
+<header>
+  <div class="container">
+    <nav class="topMenu">
+      <ul>
+        <li><a href="#">Contacto</a></li>
+        <li><a href="#">Ayuda</a></li>
+        <li><a href="#">Español</a></li>
+      </ul>
+      <div class="loginBox float-right">
+        @guest
+          <li><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
+          <li><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
+        @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+      </div>
+      <a href="{{ route('home') }}" title="Ediciones El Profesional LTDA.">
+        <img src="/img/logo-ediciones-el-profesional.png" class="logo" alt="Ediciones El Profesional LTDA.">
+      </a>
+    </nav>
+  </div>
+</header>
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+
+            </ul>
+        </div>
+    </div>
+</nav>
